12c0e90f4320bc31148bd6a032ada73bee70398e
tmserl <tom.earl@hey.com>
2022-01-29 20:29:26 +0000

    Started Heading content

--- src/components/Page/Header/index.js
+++ src/components/Page/Header/index.js
@@ -1,5 +1,15 @@
 function Header() {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <div className="header--img">
+        <img src="" alt="Tom Earl" />
+      </div>
+      <div className="header--info">
+        <h1>Hey, I'm Tom!</h1>
+        <h2>I'm a software engineer based in the West Midlands.</h2>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
