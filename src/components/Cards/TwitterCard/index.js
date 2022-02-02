fe3448e4294f5dd11e9df305da2cffb7824e716b
tmserl <tom.earl@hey.com>
2022-02-02 11:26:45 +0000

    Started styling Twitter Card

--- src/components/Cards/TwitterCard/index.js
+++ src/components/Cards/TwitterCard/index.js
@@ -1,5 +1,13 @@
+import './TwitterCard.css';
+import { ReactComponent as TwitterLogo } from './twitter-logo.svg';
+
 function TwitterCard() {
-  return <div className="card twitter--card">Twitter</div>;
+  return (
+    <div className="card twitter--card">
+      <TwitterLogo />
+      <p>I'm slowly getting back into Twitter.</p>
+    </div>
+  );
 }
 
 export default TwitterCard;
