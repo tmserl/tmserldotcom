e337a14a79291d9601e43f10a7cf2c3c7ad47af3
tmserl <tom.earl@hey.com>
2022-02-03 23:42:18 +0000

    GitHub card content

--- src/components/Cards/GitHubCard/index.js
+++ src/components/Cards/GitHubCard/index.js
@@ -1,5 +1,12 @@
 function GitHubCard() {
-  return <div className="card github--card">GitHub</div>;
+  return (
+    <div className="card github--card">
+      <p>
+        This is where all of my code lives (it's lovely and homely). Nearly all
+        of it is private for the time being, but that may change soon.
+      </p>
+    </div>
+  );
 }
 
 export default GitHubCard;
