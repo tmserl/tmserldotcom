1339ee17848072753228a0f2c87f557070e8256a
tmserl <tom.earl@hey.com>
2022-02-03 23:42:03 +0000

    Blog card content

--- src/components/Cards/BlogCard/index.js
+++ src/components/Cards/BlogCard/index.js
@@ -1,5 +1,13 @@
 function BlogCard() {
-  return <div className="card blog--card">Blog</div>;
+  return (
+    <div className="card blog--card">
+      <p>
+        I've been meaning to get into blogging for quite some time now. I may be
+        about 10 years to late, but oh well! This will be a low frequency blog
+        of both technical and non-technical ramblings.
+      </p>
+    </div>
+  );
 }
 
 export default BlogCard;
