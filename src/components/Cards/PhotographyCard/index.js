07c7a7a2c20fe5f419536467ad6da57b0afaa86f
tmserl <tom.earl@hey.com>
2022-02-03 16:12:04 +0000

    Added random colour functionality to Photography card

--- src/components/Cards/PhotographyCard/index.js
+++ src/components/Cards/PhotographyCard/index.js
@@ -1,12 +1,19 @@
 import './Photography.css';
+import { useState } from 'react';
 
 function PhotographyCard() {
+  const [color, setColor] = useState('000000');
+
+  function onHover() {
+    setColor(Math.floor(Math.random() * 16777215).toString(16));
+  }
+
   return (
-    <div className="card photography--card">
-      <h2>TomEarl.XYZ</h2>
+    <div className="card photography--card" onMouseEnter={onHover}>
+      <h2 style={{ color: `#${color}` }}>TomEarl.XYZ</h2>
       <p>
         Occassionaly, I take photos. Some of the better ones normally end up
-        here.
+        here. Lately, I'm really enjoying the Fujifilm X100V! 📷
       </p>
     </div>
   );
