6c4e8654b3817730866d15654577507b71f55c35
tmserl <tom.earl@hey.com>
2022-02-04 18:29:25 +0000

    Tweaked logic of onHover function in Photography card

--- src/components/Cards/PhotographyCard/index.js
+++ src/components/Cards/PhotographyCard/index.js
@@ -2,16 +2,22 @@ import './Photography.css';
 import { useState } from 'react';
 
 function PhotographyCard() {
-  const [color, setColor] = useState('1d2d35;');
+  const [color, setColor] = useState('1d2d35');
+  const [randomColor, setRandomColor] = useState('fcfcfc');
 
   function onHover() {
-    setColor(Math.floor(Math.random() * 16777215).toString(16));
+    setRandomColor(Math.floor(Math.random() * 16777215).toString(16));
+    setColor('fcfcfc');
   }
 
   return (
-    <div className="card photography--card" onMouseEnter={onHover}>
+    <div
+      style={{ backgroundColor: `#${randomColor}` }}
+      className="card photography--card"
+      onMouseEnter={onHover}
+    >
       <h2 style={{ color: `#${color}` }}>TomEarl.XYZ</h2>
-      <p>
+      <p style={{ color: `#${color}` }}>
         Occassionaly, I take photos. Some of the better ones normally end up
         here. Lately, I'm really enjoying the Fujifilm X100V! 📷
       </p>
