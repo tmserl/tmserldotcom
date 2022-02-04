3d18271dbc1cd564a3a635439258d6bc9669e772
tmserl <tom.earl@hey.com>
2022-02-04 20:25:05 +0000

    Added footer

--- src/components/Page/Footer/index.js
+++ src/components/Page/Footer/index.js
@@ -1,5 +1,23 @@
+import './Footer.css';
+import { useState, useEffect } from 'react';
+
 function Footer() {
-  return <div className="footer">Footer</div>;
+  const [year, setYear] = useState('');
+
+  useEffect(() => {
+    function getCurrentYear() {
+      const date = new Date();
+      setYear(date.getFullYear());
+    }
+
+    getCurrentYear();
+  }, []);
+
+  return (
+    <div className="footer">
+      <p>It's currently {year}, in case you were wondering</p>
+    </div>
+  );
 }
 
 export default Footer;
