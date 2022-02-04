bf26e6883354c0e490f7bad71c2fc5a9840ea464
tmserl <tom.earl@hey.com>
2022-02-04 18:18:08 +0000

    Further tweaks to styling

--- src/components/Cards/GitHubCard/index.js
+++ src/components/Cards/GitHubCard/index.js
@@ -1,6 +1,10 @@
+import './GitHubCard.css';
+import { ReactComponent as GitHubLogo } from './github-logo.svg';
+
 function GitHubCard() {
   return (
     <div className="card github--card">
+      <GitHubLogo />
       <p>
         This is where all of my code lives (it's lovely and homely). Nearly all
         of it is private for the time being, but that may change soon.
