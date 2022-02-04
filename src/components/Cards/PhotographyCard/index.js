d9b23316de23aca81788c0c279e336c7bd7a86be
tmserl <tom.earl@hey.com>
2022-02-04 19:24:28 +0000

    Refactored logic for random colour on hover

--- src/components/Cards/PhotographyCard/index.js
+++ src/components/Cards/PhotographyCard/index.js
@@ -1,23 +1,28 @@
-import './Photography.css';
+import './PhotographyCard.css';
 import { useState } from 'react';
 
 function PhotographyCard() {
-  const [color, setColor] = useState('1d2d35');
-  const [randomColor, setRandomColor] = useState('fcfcfc');
+  const [color, setColor] = useState(['1d2d35', 'fcfcfc']);
 
   function onHover() {
-    setRandomColor(Math.floor(Math.random() * 16777215).toString(16));
-    setColor('fcfcfc');
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    setColor([
+      ...color.slice(0, 0),
+      'fcfcfc',
+      ...color.slice(1, 1),
+      randomColor,
+      ...color.slice(2),
+    ]);
   }
 
   return (
     <div
-      style={{ backgroundColor: `#${randomColor}` }}
+      style={{ backgroundColor: `#${color[1]}` }}
       className="card photography--card"
       onMouseEnter={onHover}
     >
-      <h2 style={{ color: `#${color}` }}>TomEarl.XYZ</h2>
-      <p style={{ color: `#${color}` }}>
+      <h2 style={{ color: `#${color[0]}` }}>TomEarl.XYZ</h2>
+      <p style={{ color: `#${color[0]}` }}>
         Occassionaly, I take photos. Some of the better ones normally end up
         here. Lately, I'm really enjoying the Fujifilm X100V! 📷
       </p>
