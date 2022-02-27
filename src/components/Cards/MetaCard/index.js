6793033b5f36f774bf6f94b028884794123074c1
tmserl <tom.earl@hey.com>
2022-02-27 18:06:52 +0000

    Conditional rendering and styling

--- src/components/Cards/MetaCard/index.js
+++ src/components/Cards/MetaCard/index.js
@@ -9,14 +9,28 @@ function MetaCard() {
       <a href="https://makemeta.app" target="_blank" rel="noreferrer">
         <h2>MakeMeta</h2>
         <h3>Generate Meta Tags for your website</h3>
+      </a>
+      <div className="makemeta--card__blue">
+        <p>Try it out!</p>
         <input
+          placeholder="MakeMeta"
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
         ></input>
-        <div className="output">
-          <p>{input}</p>
-        </div>
+      </div>
+      <a href="https://makemeta.app" target="_blank" rel="noreferrer">
+        {input && (
+          <div className="makemeta--card__green">
+            <p>
+              &lt;meta name="twitter:title" content="{input}" &gt;
+              <br />
+              &lt;meta property="og:title" content={input}" &gt;
+              <br />
+              &lt;meta property="og:site_name" content="{input}" &gt;
+            </p>
+          </div>
+        )}
       </a>
     </div>
   );
