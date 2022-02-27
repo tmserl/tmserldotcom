d5fe57acbf1ae999504df18ce674dafaf0b268bc
tmserl <tom.earl@hey.com>
2022-02-27 17:29:28 +0000

    input field

--- src/components/Cards/MetaCard/index.js
+++ src/components/Cards/MetaCard/index.js
@@ -1,9 +1,23 @@
+import './MetaCard.css';
+import { useState } from 'react';
+
 function MetaCard() {
+  const [input, setInput] = useState('');
+
   return (
-    <div className="card">
-      <h2>MakeMeta</h2>
-      <h3>Generate Meta Tags for your website</h3>
-      <input></input>
+    <div className="card makemeta--card">
+      <a href="https://makemeta.app" target="_blank" rel="noreferrer">
+        <h2>MakeMeta</h2>
+        <h3>Generate Meta Tags for your website</h3>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></input>
+        <div className="output">
+          <p>{input}</p>
+        </div>
+      </a>
     </div>
   );
 }
